b408b0f6db1b7c076850b1fa23e737ece345e0d1
jameelajameel2006-code <jameelajameel2006@gmail.com>
2026-04-10 14:15:09 +0500

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,45 @@
+print("Task 1")
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
+
+
+print("Task 2")
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+ 
+print("Task 3")
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+sum=(a+b+c+d+e+f+g+h+i)
+print(sum)
+
+
+print("Task 4")
+import math
+print("Area and perimeter of the circle")
+radius= 5.5
+perimeter = 2 * radius * math.pi
+area= radius * radius * math.pi
+print("Radius of circle",radius)
+print("Area of circle",area)
+print("Perimeter of circle")
+
+
+
+		
+
+
