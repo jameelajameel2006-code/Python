473abc050412af3cf1688c8edd400155d9647c80
jameelajameel2006-code <jameelajameel2006@gmail.com>
2026-04-10 14:17:45 +0500

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,86 @@
+name=input("Enter your name:")
+city=input("Enter uour city:")
+favorite_subject = input("Enter your favorite subject:")
+print("Hellow",name,"you live in ",city,"your favorite subject is ",favorite_subject)
+
+
+first_word=input("Enter first word:")
+second_word=input("Enter second word:")
+print(first_word,second_word)
+
+
+a=input("Username entered:")
+username="admin"
+b="admin"
+c=username==b
+print(username,'==',b,'',c)
+
+
+
+sentence=("I am learning python programming")
+word=input("Enter a word:")
+print("python" in sentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
