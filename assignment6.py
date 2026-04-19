f0e21dbbaab7bf9f0981e690a9c4cc86d5278a7d
jameelajameel2006-code <jameelajameel2006@gmail.com>
2026-04-18 22:42:06 -0700

    Add files via upload

---
+++ assignment6.py
@@ -0,0 +1,10 @@
+num=[5,10,15,20,25]
+print("First element:",num[0])
+print("Last element:",num[-1])
+print("All elements:",num)
+for i in num:
+	print(i)
+print("Numbers greater than 10:")
+for i in num:
+	if i>10:
+		print(i)
