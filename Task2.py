0e352e49b9f569196c65812ca4947c3e3612ddb1
jameelajameel2006-code <jameelajameel2006@gmail.com>
2026-05-03 22:33:28 +0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,43 @@
+#Define the menu of restaurant
+menu = {
+    'Pizza':450,
+    'Pasta':400,
+    'Burger':500,
+    'Salad':150,
+    'Coffee':200
+}
+
+#Greet
+print("Welcome to PYTHON Restaurant")
+print("Pizza: Rs450\nPasta: Rs400\nBurger: Rs500\nSalad: Rs150\nCoffee Rs200")
+
+order_total = 0
+#450 + 400 =850
+
+item_1 = input("Enter the name of item you want to order =")
+if item_1 in menu:
+    order_total += menu[item_1] #0 + 50
+    print(f"Your item {item_1} has been added to your order")
+
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+
+another_order = input("Do you want to add another item? (Yes\\No)")
+if another_order == "Yes":
+    item_2 = input("Enter the name of second item =")
+    if item_2 in menu:
+        order_total += menu[item_2]
+        print(f"Item {item_2} has been added to order")
+    else:
+        print(f"Ordered item {item_2} is not available!")
+
+print(f"The total amount of items is {order_total}")
+
+
+
+
+
+
+
+
+
